83afb0b16c5c9a34c8a3f85b4dc9b2df3bc0d808
Azhar711 <azharali368@gmail.com>
2018-08-27 20:25:02 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { BodyComponent } from './body/body.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
